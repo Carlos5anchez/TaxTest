743431c3127a881a68d7aaedbbb78c40fddca4ad
Carlos Manuel Sanchez Martinez <43074684+Carlos5anchez@users.noreply.github.com>
2021-01-23 01:55:23 -0600

    aaaaa

--- static/js/signup.js
+++ static/js/signup.js
@@ -5,7 +5,7 @@ document.getElementById("registro-input").addEventListener("click",redirect)
 document.getElementById("signupButton").addEventListener("click",signup)
 
 function redirect(){
-    window.location.replace("index.html")
+    window.location.replace("index.php")
 }
 
 function signup(){
