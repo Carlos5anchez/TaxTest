a4c31c0babbd4ee7561c34eeead99c00749707cf
Carlos Manuel Sanchez Martinez <43074684+Carlos5anchez@users.noreply.github.com>
2021-01-23 02:01:03 -0600

    aphp

--- static/js/signup.js
+++ static/js/signup.js
@@ -44,7 +44,7 @@ function signup(){
             if(localStorage.getItem(rfc) === null){
                 localStorage.setItem(rfc, `${name},${mail},${pass}`);
                 alert("Registro Completo")
-                 window.location.replace("index.html")
+                 window.location.href("index.php")
 
             }
             else{
